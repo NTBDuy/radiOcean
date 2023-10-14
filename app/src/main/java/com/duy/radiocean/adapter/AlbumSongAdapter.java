e9200a69fedb766aa6c253abcb67d690ec94eb84
macs <duyntb2001@gmail.com>
2023-10-14 23:36:31 +0700

    14102023 2335 Duy

--- app/src/main/java/com/duy/radiocean/adapter/AlbumSongAdapter.java
+++ app/src/main/java/com/duy/radiocean/adapter/AlbumSongAdapter.java
@@ -34,7 +34,7 @@ public class AlbumSongAdapter extends RecyclerView.Adapter<AlbumSongAdapter.View
 
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_listitem, parent, false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_list_album, parent, false);
         return new ViewHolder(view);
     }
 
