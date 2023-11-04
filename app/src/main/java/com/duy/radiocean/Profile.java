44334932a941d39523aa95f11413a6657cdbf47c
VinhQuangPham <pmwangvinh@gmail.com>
2023-11-04 15:52:50 +0700

    authentication(Vinh Farm made it)

--- app/src/main/java/com/duy/radiocean/Profile.java
+++ app/src/main/java/com/duy/radiocean/Profile.java
@@ -2,11 +2,36 @@ package com.duy.radiocean;
 
 public class Profile {
     String email, name, gender;
+    String id;
 
-    public Profile(String email, String name, String gender) {
+    public String getId() {
+        return id;
+    }
+
+    public Profile(String id, String email, String name, String gender) {
+        this.id = id;
         this.email = email;
         this.name = name;
         this.gender = gender;
+    }
+    public Profile(){
+
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
     public String getEmail() {
